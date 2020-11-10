2d005acf122527eb10b88fba8c1dcb0960b80dd4
SUCHMOKUO <suchmokuo@gmail.com>
2020-11-10 20:14:16 +0800

    finish p1 & p2 & p3

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,25 @@
-export const getPoetry
+// export const getPoetry = async () => {
+//   const response = await fetch("https://v1.jinrishici.com/all.json");
+
+//   if (!response.ok) {
+//     throw new Error(response.statusText);
+//   }
+
+//   const { origin, author, content } = await response.json();
+
+//   return [origin, author, content];
+// };
+
+export const getPoetry = () => {
+  return fetch("https://v1.jinrishici.com/all.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const { origin, author, content } = data;
+      return [origin, author, content];
+    });
+};
